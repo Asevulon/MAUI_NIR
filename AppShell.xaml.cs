f6f633351adfba01f04d59f31aa0017883a4a983
Asevulon <Asevulon@yandex.ru>
2023-12-21 02:22:38 +0300

    Добавьте файлы проекта.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUI_NIR;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MAUI_NIR;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		CounterBtn.Text = $"{count} нажатий!";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private void OnResetClicked(object sender, EventArgs e)
+	{
+		count = 0;
+		CounterBtn.Text = "Нажми меня!";
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
